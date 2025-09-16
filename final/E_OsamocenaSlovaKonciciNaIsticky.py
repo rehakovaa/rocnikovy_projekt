d1a085b6f0b8514b5a64376be7837edd6b4ad8d5
Aneta Řeháková <93251126+rehakovaa@users.noreply.github.com>
2025-09-16 14:54:31 +0200

    dodán popis testu E_OsamocenaSlovaKonciciNaIsticky.py

--- final/E_OsamocenaSlovaKonciciNaIsticky.py
+++ final/E_OsamocenaSlovaKonciciNaIsticky.py
@@ -1,3 +1,8 @@
+""" 
+test hledá slova končící na 'istický', která jsou bez rodiče a snaží se ho dohledat
+
+"""
+
 import derinet.lexicon as dlex
 import os
 
@@ -30,25 +35,20 @@ all_lemmas = {lex.lemma for lex in lexicon.iter_lexemes()}
 
 for lexeme in lexicon.iter_lexemes():
     if lexeme.lemma.endswith("istický") and lexeme.parent is None:
+       for lexeme in lexicon.iter_lexemes():
+    if lexeme.lemma.endswith("istický") and lexeme.parent is None:
         if not lexeme.lemma.endswith("statistický") and not lexeme.lemma.startswith("proti"):
             nove = lexeme.lemma[:-6]
             nove = nove + "smus"
-            if nove in all_lemmas:
-                vysledek.append((lexeme.lemma, nove))
-            else:
-                neexistuje.append(lexeme.lemma)
-
-        if lexeme.lemma.endswith("statistický"):
-            vysledek.append((lexeme.lemma, "statistika"))
-        
-        if lexeme.lemma.startswith("proti"):
-
+        elif lexeme.lemma.startswith("proti"):
             nove = lexeme.lemma[:-6][5:]
             nove = nove + "smus"
-            if nove in all_lemmas:
-                vysledek.append((lexeme.lemma, nove))
-            else:
-                neexistuje.append(lexeme.lemma)
+
+        if nove in all_lemmas:
+            vysledek.append((lexeme.lemma, nove))
+        else:
+            neexistuje.append(lexeme.lemma)
 
 tisk(vysledek, neexistuje)
+
 
