00803cd655ebdcf6384d4980e2dcf2ccb03da266
Aneta Řeháková <93251126+rehakovaa@users.noreply.github.com>
2025-09-16 15:14:53 +0200

    dodán popis testu a jedna z větví E_OsamocenaSlovaKonciciNaUv.py

--- final/E_OsamocenaSlovaKonciciNaUv.py
+++ final/E_OsamocenaSlovaKonciciNaUv.py
@@ -1,3 +1,15 @@
+""" 
+test hledá přídavná jména končící na 'ův' a kontroluje několik vlastností: 
+    - slovo má předchůdce odpovídající tvaru (srncův -> srnec anebo tesařův -> tesař anebo antinacistův -> antinacista), ale není mužského rodu
+    - slovo nemá předchůdce odpovídající tomuto tvaru
+        - existuje takové slovo
+        - existuje takové slovo, ale není mužského rodu
+        - neexistuje takové slovo
+
+upozornění:
+existují případy, kdy je antinacistův pod nacistův
+"""
+
 import derinet.lexicon as dlex
 import os
 
@@ -21,16 +33,22 @@ def tisk_koncovky(spatny_rod, dobry_rod, spatny_nepripojeno, bez, rod, koncovka,
         for i in bez:
             f.write(f"{i} \n")
 
+lexicon = dlex.Lexicon()
+current_dir = os.getcwd()  # aktualni adresar
+file_path = os.path.join(current_dir, "./derinet-2-3.tsv")  #sestaveni cesty
+lexicon.load(file_path)
+
 def tvorba_predka(slovo):
     temp = slovo[:-2]
     nove = temp[0:-1]
     nove2 = temp[-1]
     slovicko = nove + "e" + nove2
-    return slovicko
+    slovicko1 = slovo[:-2] + "a"
+    return slovicko, slovicko1
     
 def hledani_predka(lexeme):
     rodice = lexeme.all_parents
-    pomocnik = tvorba_predka(lexeme.lemma)
+    pomocnik1, pomocnik2 = tvorba_predka(lexeme.lemma)
     jeden = False # jestli jsme našli nějakou předkyni
     jiny = False # pokud jsme ale nasli predka, co neni muzsky, ale sedí do popisu
     slovo = None
@@ -40,7 +58,7 @@ def hledani_predka(lexeme):
             re = r.lemma[:-1]
         if re.endswith("os"):
             re = r.lemma[:-2]
-        if r and (re == pomocnik or re == lexeme.lemma[:-2]):
+        if r and (re == pomocnik1 or re==pomocnik2 or re == lexeme.lemma[:-2]):
             jiny = True 
             if r.pos == "NOUN" and "Gender" in r.feats.keys() and r.feats["Gender"] == "Masc":
                 jeden = True
@@ -49,14 +67,6 @@ def hledani_predka(lexeme):
                 slovo = r.lemma
 
     return jeden, jiny, slovo
-
-
-lexicon = dlex.Lexicon()
-current_dir = os.getcwd()  # aktualni adresar
-file_path = os.path.join(current_dir, "./derinet-2-3.tsv")  #sestaveni cesty
-lexicon.load(file_path)
-
-
 
 all_lemmas = {lex.lemma for lex in lexicon.iter_lexemes()}
 spatny_rod, dobry_rod, spatny_nepripojeno, bez = [], [], [], []#špatný rod - předek není ženský 
@@ -73,10 +83,12 @@ for lexeme in lexicon.iter_lexemes():
             spatny_rod.append((lexeme.lemma, slovo))
         elif not jeden and not jiny:
 
-            pomocnik = tvorba_predka(lexeme.lemma)
+            pomocnik1, pomocnik2 = tvorba_predka(lexeme.lemma)
             lex = None
-            if pomocnik in all_lemmas:
-                lex = lexicon.get_lexemes(pomocnik)[0]
+            if pomocnik1 in all_lemmas:
+                lex = lexicon.get_lexemes(pomocnik1)[0]
+            elif pomocnik2 in all_lemmas:
+                lex = lexicon.get_lexemes(pomocnik2)[0]
             elif lexeme.lemma[:-2] in all_lemmas:
                 lex = lexicon.get_lexemes(lexeme.lemma[:-2])[0]
 
@@ -88,5 +100,4 @@ for lexeme in lexicon.iter_lexemes():
             else:
                 bez.append(lexeme.lemma)
 
-print(len(dobry_rod) + len(spatny_nepripojeno), len(bez))    
 tisk_koncovky(spatny_rod, dobry_rod, spatny_nepripojeno, bez, "MUŽSKÉHO", "ŮV", "E_OsamocenaSlovaKonciciNaUv")
