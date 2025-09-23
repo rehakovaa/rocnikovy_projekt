86a29a3b5b787cea4c0b41811c16303af144a033
Aneta Řeháková <93251126+rehakovaa@users.noreply.github.com>
2025-09-23 03:39:39 +0200

    rozdělen výpis do dvou souborů, oba tsv formát
    jeden soubor se věnuje izmům, která jsou bez rodiče a druhý soubor se věnuje istický, který je pod izmus, ale ne ismus

--- final/W_VztahyIzmu.py
+++ final/W_VztahyIzmu.py
@@ -3,31 +3,23 @@ test se zabývá slovy, které končí na 'izmus' a zkoumá u nich několik vlat
 - zda je slovo bez rodiče, a pokud ano, tak zda pro něj existuje verze, kdy slovo končí na 'ismus' 
 - a zda slovo nemá potomka končící na 'ista', a pokud ano, tak zda neexistuje verze 'ismus', pod který by mělo být to dítě připojeno
 """
-
 import derinet.lexicon as dlex
 import os
+import opakovane_funkce
 
 
 lexicon = dlex.Lexicon()
 current_dir = os.getcwd()  # aktualni adresar
 file_path = os.path.join(current_dir, "./derinet-2-3.tsv")  #sestaveni cesty
 lexicon.load(file_path)
-def tisk(ismy, jenom_jeden):
-    with open("W_VztahyIzmu.txt", "w", encoding="utf-8") as f: #kdyz tam dodam with, tak se mi to samo zavre¨
-        f.write("TEST ZABÝVAJÍCÍ SE VZTAHY MEZI -IZMUS, -ISMUS A -ISTICKÝ\n")
-        f.write("SEZNAM SLOV, KTERÉ KONČÍ NA 'IZMUS', ALE NEJSOU PŘIPOJENA K VERZI 'ISMUS'\n")
-        f.write(f"{'-IZMUS'.ljust(30)}{'-ISMUS'.ljust(30)}\n")
-        f.write("\n")
-        for i in ismy: 
-            f.write(f"{i[0].ljust(30)}{i[1].ljust(30)}\n")
-        f.write("\n")
 
-        f.write("SEZNAM SLOV KONČÍCÍ NA 'ISTICKÝ', KTERÁ JSOU PŘIPOJENA NA 'IZMUS', ALE NE 'ISMUS\n")
-        f.write(f"{'-ISTICKÝ'.ljust(30)}{'ŠPATNĚ: -IZMUS'.ljust(30)}{'SPRÁVNĚ: -ISMUS'.ljust(30)}\n")
-        f.write("\n")
+def tisk(jenom_jeden):
+    with open("W_VztahyIsticky.tsv", "w", encoding="utf-8") as f: #kdyz tam dodam with, tak se mi to samo zavre¨
+        f.write("*slova končící na 'istický!, která jsou pod 'izmus', ale ne pod 'ismus'\n")
+        f.write(f"*istický\tizmus\tismus\n")
+        f.write("*\n")
         for i in jenom_jeden: 
-            f.write(f"{i[1].ljust(30)}{i[0].ljust(30)}{i[2].ljust(30)}\n")
-        f.write("\n")
+            f.write(f"*{i[1]}\t[{i[0]}\t{i[2]}\n")
 
     
 seznam = [] #když masochizmus není příbuzný masochismu
@@ -59,7 +51,6 @@ for lexeme in lexicon.iter_lexemes():
                     if rodic not in jmena: #pokud má to dítě dva rodiče - masochista má jak masochizmus a masochismus
                         nespojeno.append((lexeme.lemma, d.lemma, rodic))
                 
-tisk(seznam, nespojeno)
-
-
+tisk(nespojeno)
+opakovane_funkce.vypis_jeden_seznam(seznam, "E_VztahyIzmu.tsv", "slova končící na 'izmus' bez rodiče")
 
