67532c17d37f9427d2b1c65b204dc74a46528a4a
Aneta Řeháková <93251126+rehakovaa@users.noreply.github.com>
2025-09-23 02:35:37 +0200

    odstraněny opakující se funkce a přepsán výtisk

--- final/E_OsamocenaKoncovkaOst.py
+++ final/E_OsamocenaKoncovkaOst.py
@@ -2,10 +2,9 @@
 tento test hledá podstatná jména končící na 'ost', která nemají rodiče. jelikož tento test míří na podstatná jména, která vznikla od přídavného jména, je dodáno omezení
 že takovéto slova musí být delší než čtyři písmena (aby se předešlo slovům jako je most)
 """
-
 import derinet.lexicon as dlex
 import os
-
+import opakovane_funkce
 
 lexicon = dlex.Lexicon()
 current_dir = os.getcwd()  # aktualni adresar
@@ -26,23 +25,7 @@ def hledani(slovo):
     else:
         return None
     
-def tisknuti(seznam, osamocen):
-    with open("E_OsamocenaKoncovkaOst.txt", "w", encoding="utf-8") as f: #kdyz tam dodam with, tak se mi to samo zavre
-        f.write("PODSTATNÁ JMÉNA KONČÍCÍ NA 'OST' A JSOU BEZ PŘEDKA \n")
-        f.write("PODSTATNÉ JMÉNO A K NĚMU NALEZENÝ PŘEDEK\n")
-        
-        f.write(f"{'PODSTATNÉ JMÉNO'.ljust(20)}{'PŘEDEK'.ljust(20)}\n")
-        for i in seznam:
-            f.write(f"{i[0].ljust(20)}{i[1].ljust(20)}\n")
-
-        f.write("\n")
-        f.write("PODSTATNÁ JMÉNA, PRO NĚŽ NEBYL NALEZEN PŘEDCHŮDCE\n")
-        f.write("SEŘAZENO PODLE ABSOLUTNÍHO POČTU Z lexeme.misc \n")
-        for slovo, vzdalenost in sorted(osamocen, key=lambda x: x[1]):
-            f.write(f"{slovo.ljust(20)}{str(vzdalenost).ljust(20)}\n")
-        
-        
-    
+     
 seznam = []
 bez = []
 for lexeme in lexicon.iter_lexemes():
@@ -64,7 +47,7 @@ for lexeme in lexicon.iter_lexemes():
                     hodnota = lexeme.misc.get("corpus_stats", {}).get("absolute_count", 0)
                     bez.append((lexeme.lemma, hodnota))
 
-tisknuti(seznam, bez)
 
+opakovane_funkce.vypis_dva_seznamy(seznam, sorted(bez, key=lambda x: x[1]),"E_OsamocenaKoncovkaOst.txt", "opuštěná slova končící na 'ost'")
 
 
