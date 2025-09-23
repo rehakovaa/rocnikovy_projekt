79b0c6a17ec8f16bdef1050e2dc0da562dd6f753
Aneta Řeháková <93251126+rehakovaa@users.noreply.github.com>
2025-09-23 02:53:40 +0200

    výpis převeden do jiného souboru

--- final/E_OsamocenaSlovaKonciciNaCin.py
+++ final/E_OsamocenaSlovaKonciciNaCin.py
@@ -9,6 +9,8 @@ jelikož je to dobře popsatelná skupina, v testu se kontroluje několik možno
 """
 import derinet.lexicon as dlex
 import os
+import opakovane_funkce
+
 
 lexicon = dlex.Lexicon()
 current_dir = os.getcwd()  # aktualni adresar
@@ -17,56 +19,11 @@ lexicon.load(file_path)
 
 all_lemmas = {lex.lemma for lex in lexicon.iter_lexemes()}
 
-def tisk_koncovky(spatny_rod, dobry_rod, spatny_nepripojeno, bez, rod, koncovka, soubor, vzdalene):
-    with open(f"{soubor}.txt", "w", encoding="utf-8") as f:
-        f.write(f"PŘÍDAVNÁ JMÉNA KONČÍCÍ NA -{koncovka}, KTERÁ NEJSOU PŘIPOJENA KE SLOVU {rod} RODU \n")
-        f.write(f"{'PŘÍDAVNÉ JMÉNO'.ljust(20)}{'PODSTATNÉ JMÉNO'.ljust(20)}\n")
-        f.write("\n")
-        f.write(f"JE PŘIPOJENO KE SLOVU, KTERÉ NENÍ {rod} RODU\n")
-        for i in spatny_rod:
-            f.write(f"{i[0].ljust(20)}{i[1].ljust(20)}\n")
-        f.write("\n")
-        f.write("JSOU VZDÁLENĚ PŘÍBUZNÍ\n")
-        f.write(f"{'VZDÁLENÝ SYN'.ljust(20)}{'VZDÁLENÝ PŘEDEK'.ljust(20)}\n")
-        for i in vzdalene:
-            f.write(f"{i[0].ljust(20)}{i[1].ljust(20)}\n")
-        f.write("\n")
-        f.write(f"NENÍ PŘIPOJENO K PŘEDKOVI, KTERÝ JE {rod} RODU \n")
-        for i in dobry_rod:
-            f.write(f"{i[0].ljust(20)}{i[1].ljust(20)}\n")
-        f.write(f"EXISTUJE NEPŘIPOJENÝ PŘEDEK, KTERÝ NENÍ {rod} RODU\n")
-        for i in spatny_nepripojeno:
-            f.write(f"{i[0].ljust(20)}{i[1].ljust(20)}\n")
-        f.write("NEBYL NALEZEN PŘEDEK \n")
-        for i in bez:
-            f.write(f"{i} \n")
-
-def vzdalene_pribuzny(dite, rodic):
-    daleko = False
-    if dite.all_parents != []:
-        for i in dite.all_parents:
-            if lezeme_nahoru(i, rodic):
-                daleko = True
-    if daleko:
-        return True
-    return False
-
-def lezeme_nahoru(dite, hledany): #podívám se, jestli nejsou nějak propojené
-    rodice = dite.all_parents
-    if not rodice:
-        return False  # nedošli jsme k hledanému
-    if hledany in rodice:
-        return True  # hledaný je přímo rodičem
-    for r in rodice:
-        if lezeme_nahoru(r, hledany):  # rekurzivně jdeme dál
-            return True
-    return False  # pokud jsme ho nikdy nenašli
-
 
 spatny_rod, dobry_rod, spatny_nepripojeno, bez = [], [], [], []#špatný rod - předek není ženský 
 #dobrý rod - není připojen, ale je ženský, spatny_nepripojeno - nepřipojený předek a ještě není ženský
 # bez - bez predka
-vzdalene = [] #pokud nejsou hned nad sebou, ale vzdaleně příbuzní
+vzdalene = []
 
 for lexeme in lexicon.iter_lexemes():
     if lexeme.lemma.endswith("čin") and lexeme.pos == "ADJ":
@@ -90,9 +47,9 @@ for lexeme in lexicon.iter_lexemes():
             rodic = lexeme.lemma[:-3] + "ka"
             if rodic in all_lemmas:
                 predek = lexicon.get_lexemes(rodic)[0]
-                if vzdalene_pribuzny(lexeme, predek):
+                if opakovane_funkce.vzdalene_pribuzny(lexeme, predek):
                     vzdalene.append((lexeme.lemma, predek.lemma))
-                elif vzdalene_pribuzny(predek, lexeme):
+                elif opakovane_funkce.vzdalene_pribuzny(predek, lexeme):
                     vzdalene.append((predek.lemma, lexeme.lemma))
                 elif predek.pos == "NOUN" and predek.feats["Gender"] == "Fem":
                     dobry_rod.append((lexeme.lemma, predek.lemma))
@@ -100,10 +57,6 @@ for lexeme in lexicon.iter_lexemes():
                     spatny_nepripojeno.append((lexeme.lemma, predek.lemma))
             else:
                 bez.append(lexeme.lemma)
-            
-tisk_koncovky(spatny_rod, dobry_rod, spatny_nepripojeno, bez, "ŽENSKÉHO", "ČIN", "E_OsamocenaSlovaKonciciNaCin", vzdalene)
+             
+opakovane_funkce.vypis_matcin_otcuv(spatny_rod, dobry_rod, spatny_nepripojeno, bez, "ženského", "čin", "E_OsamocenaSlovaKonciciNaCin", vzdalene)
                     
-
-
-
-
