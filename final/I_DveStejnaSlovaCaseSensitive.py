3f3ff606153eae3fd82d2d5949d02e8434970607
Aneta Řeháková <93251126+rehakovaa@users.noreply.github.com>
2025-09-23 03:23:35 +0200

    výpis převeden do knihovny a teď je tsv formátu

--- final/I_DveStejnaSlovaCaseSensitive.py
+++ final/I_DveStejnaSlovaCaseSensitive.py
@@ -3,35 +3,14 @@ test hledá lexémy, které jsou identické, ale ani jeden z nich nemá rodiče
 identické ve smyslu, že první písmeno je stejně velké 
 výtisk se dělí podle toho, jestli lexémy sdílí stejný slovní druh a každá kategorie má vlastní soubor
 """
-
-
 import derinet.lexicon as dlex
 import os
+import opakovane_funkce
+
 lexicon = dlex.Lexicon()
 current_dir = os.getcwd()  # aktualni adresar
 file_path = os.path.join(current_dir, "./derinet-2-3.tsv")  #sestaveni cesty
 lexicon.load(file_path)
-
-#testování 1
-def tisknuti(veta, soubor, seznam):
-
-    # -------------------- výstup --------------------
-    with open(f"{soubor}.txt", "w", encoding="utf-8") as f:
-        f.write(f"{veta}\n")
-        f.write(f"{'SLOVO 1'.ljust(20)}{'SLOVO 2'.ljust(20)}{'SLOVNÍ DRUH'.ljust(10)}\n")
-        f.write("\n")
-
-        zmena = "velké"
-        for i in sorted(seznam, key=lambda x: x[0]):
-            if i[0][0].islower() and zmena == "velké":
-                f.write("\n")
-                zmena = "malé"
-            elif i[0][0].isupper() and zmena == "malé":
-                f.write("\n")
-                zmena = "velké"
-            
-            f.write(f"{i[0].ljust(20)}{i[1].ljust(20)}{i[2].ljust(10)}\n")
-
 
 videno = set()
 stejny_typ, ruzny_typ = set(), set()
@@ -51,7 +30,5 @@ for lexeme in lexicon.iter_lexemes():
                         stejny_typ.add((lexeme.lemma, lex.lemma, lexeme.pos, lex.pos))
                         #f.write(f"{lexeme.lemma}, {lex.lemma} - STEJNÝ typ: {lex.pos} \n")
 
-tisknuti("TEST: IDENTICKÁ SLOVA, KTERÁ K SOBĚ NAVZÁJEM NEJSOU NIJAK PŘIPOJENA A MAJÍ STEJNÝ SLOVNÍ DRUH","I_DveStejnaSlovaCaseSensitiveA", stejny_typ)
-tisknuti("TEST: IDENTICKÁ SLOVA, KTERÁ K SOBĚ NEJSOU PŘIPOJENA A NESDÍLÍ STEJNÝ SLOVNÍ DRUH","I_DveStejnaSlovaCaseSensitiveB", ruzny_typ)
-
-
+opakovane_funkce.vypis_identicka_slova("identická slova se stejným slovním druhem","I_DveStejnaSlovaCaseSensitiveA", stejny_typ)
+opakovane_funkce.vypis_identicka_slova("identická slova s různým slovním druhem","I_DveStejnaSlovaCaseSensitiveB", ruzny_typ)
