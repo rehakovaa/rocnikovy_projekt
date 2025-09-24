cbda94a51a4ebab2f349da8f54be6a82dd93099f
Aneta Řeháková <93251126+rehakovaa@users.noreply.github.com>
2025-09-24 21:12:03 +0200

    zjednodušení programu

--- final/spousteni_testu.py
+++ final/spousteni_testu.py
@@ -1,9 +1,7 @@
 import os
-import requests
-import urllib.request
 import importlib.util
 import derinet.lexicon as dlex
-import testy
+
 
 def nalezeni_a_spousteni_testu():
     # cesta k adresáři, kde je spuštěn tento skript
@@ -17,7 +15,7 @@ def nalezeni_a_spousteni_testu():
 
     adresar_testy = os.path.join(cesta_adresar, "test")
     # správná cesta ke složce testů
-    
+
     jmena = [f for f in os.listdir(adresar_testy) if f.startswith(("E_", "W_", "I_")) and f.endswith(".py")]
     
     for test in jmena:
@@ -37,19 +35,10 @@ def nalezeni_a_spousteni_testu():
     print("Hotovo, všechny testy byly spuštěny.")
 
 # DeriNet
-derinet_url = "https://lindat.mff.cuni.cz/repository/bitstreams/6fa2bce5-51cd-42a9-ba70-cb640542cd13/download"
 derinet_jmeno = "derinet-2-3.tsv"
 
 cesta_adresar= os.path.dirname(os.path.abspath(__file__))
 
 cesta_derinet = os.path.join(cesta_adresar, derinet_jmeno)
-
-
-if not os.path.isfile(cesta_derinet):
-    print(f"soubor {derinet_jmeno} není ve stejném adresáři, stahuji")
-    print(f"{derinet_jmeno} chybí, stahuji...")
-
-else:
-    print(f"soubor {derinet_jmeno} je ve stejném adresáři, nemusím stahovat")
 
 nalezeni_a_spousteni_testu()
