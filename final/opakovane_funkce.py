e2ba90bfed6cb1e808762e0b8378f38816f93809
Aneta Řeháková <93251126+rehakovaa@users.noreply.github.com>
2025-09-24 05:30:37 +0200

    přepsány detaily ve funkcích a několik dodáno

--- final/opakovane_funkce.py
+++ final/opakovane_funkce.py
@@ -1,88 +1,106 @@
-def vypis_identicka_slova(veta, soubor, seznam):
-        # -------------------- výstup --------------------
-    with open(f"{soubor}.sv", "w", encoding="utf-8") as f:
+import os
+
+def hledani_cesty(jmeno):
+    # absolutní cesta do stejného adresáře, kde leží tento skript
+    adresar = os.path.dirname(os.path.abspath(__file__))
+    vystup = os.path.join(adresar, jmeno)
+    return vystup
+
+def vypis_identicka_slova(veta, soubor, seznam, varianta):
+    soubor = soubor + ".tsv"
+    with open(hledani_cesty(soubor), "w", encoding="utf-8") as f:
         f.write(f"*{veta}\n")
-        f.write(f"*zkoumané slovo\tnalezené slovo'\n")
+        f.write(f"*{varianta}\tzkoumané slovo\tnalezené slovo\n")
         f.write("*\n")
 
-        zmena = "velké"
-        for i in sorted(seznam, key=lambda x: x[0]):
-            if i[0][0].islower() and zmena == "velké":
-                f.write("*\n")
-                zmena = "malé"
-            elif i[0][0].isupper() and zmena == "malé":
-                f.write("*\n")
-                zmena = "velké"
-            
-            f.write(f"{i[0]}\t{i[1]}\n") 
+        for i in sorted(seznam, key=lambda x: x[0]):            
+            f.write(f"{varianta}\t{i[0]}\t{i[1]}\n") 
 
-def vypis_tri_seznamy(seznam_dobry, seznam_spatny, bez, soubor, popis):
-    with open(soubor, "w", encoding="utf-8") as f: #kdyz tam dodam with, tak se mi to samo zavre
+def levenhstein(prvni, druhy):
+    n = len(prvni)
+    m = len(druhy)
+    tabulka = [[0] * (m + 2) for _ in range(n + 2)]
+    for i in range(1, n+ 2):
+        tabulka[i][m+1] = n - i + 1
+    for j in range(1, m + 2):
+        tabulka[n+ 1][j] = m - j + 1
+    for i in range(n, 0, -1):
+        for j in range(m, 0, -1):
+            delta = 1
+            if prvni[i - 1] == druhy[j-1]:
+                delta = 0
+            tabulka[i][j] = min(delta + tabulka[i + 1][j + 1], 1 + tabulka[i + 1][j], 1 + tabulka[i][j + 1])
+
+    return tabulka[1][1]
+
+def vypis_tri_seznamy(seznam_dobry, seznam_spatny, bez, soubor, popis, varianta):
+    with open(hledani_cesty(soubor), "w", encoding="utf-8") as f: #kdyz tam dodam with, tak se mi to samo zavre
         f.write("*" + popis + "\n")
         f.write("*tři skupiny: nalezený rodič, který je častější než nalezené slovo, nalezený rodič, který je méně častý než nalezené slovo, nenalezen rodič\n")
-        f.write(f"*zkoumané slovo\tnalezené slovo'\n")
+        f.write(f"*{varianta}\tzkoumané slovo\tnalezené slovo\n")
         f.write("*\n")
         f.write(f"*častější předek\n")
         for i in seznam_dobry:
-            f.write(f"{i[0]}\t{i[1]}\n")  
+            f.write(f"{varianta}\t{i[0]}\t{i[1]}\n")  
         f.write("*\n")
         f.write("*méně častý předek\n")
         for i in seznam_spatny:
-            f.write(f"{i[0]}\t{i[1]}\n")    
+            f.write(f"{varianta}\t{i[0]}\t{i[1]}\n")    
         f.write("*\n")
         f.write("*nenalezen předek\n")
         for i in bez:
-            f.write(f"{i}\n")
+            f.write(f"{varianta}\t{i}\n")
 
-def vypis_jeden_seznam(seznam, soubor, popis):
-    with open(soubor, "w", encoding="utf-8") as f: #kdyz tam dodam with, tak se mi to samo zavre
+def vypis_jeden_seznam(seznam, soubor, popis, varianta):
+    with open(hledani_cesty(soubor), "w", encoding="utf-8") as f: #kdyz tam dodam with, tak se mi to samo zavre
         f.write("*" + popis + "\n")
-        f.write(f"*zkoumané slovo\tnalezené slovo'\n")
+        f.write(f"*{varianta}\tzkoumané slovo\tnalezené slovo\n")
         for i in seznam: 
-            f.write(f"{i[0]}\t{i[1]}\n")
+            f.write(f"{varianta}\t{i[0]}\t{i[1]}\n")
 
-def vypis_jeden_seznam_vic_moznosti(seznam, soubor, popis):
-    with open(soubor, "w", encoding="utf-8") as f: #kdyz tam dodam with, tak se mi to samo zavre
+def vypis_jeden_seznam_vic_moznosti(seznam, soubor, popis, varianta):
+    with open(hledani_cesty(soubor), "w", encoding="utf-8") as f: #kdyz tam dodam with, tak se mi to samo zavre
         f.write("*" + popis + "\n")
-        f.write(f"*zkoumané slovo\tnalezené slovo'\n")
+        f.write(f"*{varianta}\tzkoumané slovo\tnalezené slovo\n")
         for slovo, deti in seznam:
             for i in deti: 
-                f.write(f"{slovo}\t{i}\n")
+                f.write(f"{varianta}\t{slovo}\t{i}\n")
 
-def vypis_dva_seznamy(seznam, bez, soubor, popis):
-    with open(soubor, "w", encoding="utf-8") as f: #kdyz tam dodam with, tak se mi to samo zavre
+def vypis_dva_seznamy(seznam, bez, soubor, popis, varianta):
+    with open(hledani_cesty(soubor), "w", encoding="utf-8") as f: #kdyz tam dodam with, tak se mi to samo zavre
         f.write("*" + popis + "\n")
         f.write("*dvě skupiny: nalezený rodič a nenalezený rodič\n")
-        f.write(f"*zkoumané slovo\tnalezené slovo'\n")
+        f.write(f"*{varianta}\tzkoumané slovo\tnalezené slovo\n")
         f.write("*\n")
         f.write(f"*nalezen předek\n")
         for i in seznam:
-            f.write(f"{i[0]}\t{i[1]}\n")  
+            f.write(f"{varianta}\t{i[0]}\t{i[1]}\n")  
         f.write("*\n")
         f.write("*nenalezen předek\n")
         for i in bez:
-            f.write(f"{i}\n")
+            f.write(f"{varianta}\t{i}\n")
 
-def vypis_matcin_otcuv(spatny_rod, dobry_rod, spatny_nepripojeno, bez, rod, koncovka, soubor, vzdalene):
-    with open(f"{soubor}.txt", "w", encoding="utf-8") as f:
+def vypis_matcin_otcuv(spatny_rod, dobry_rod, spatny_nepripojeno, bez, rod, koncovka, soubor, vzdalene, varianta1, varianta2):
+    soubor = soubor + ".tsv"
+    with open(hledani_cesty(soubor), "w", encoding="utf-8") as f:
         f.write(f"*přídavná jména končící na '{koncovka}', která nejsou připojena ke slovu {rod} rodu\n")
-        f.write(f"*zkoumané slovo\tnalezené slovo'\n")
+        f.write(f"*varianta\tzkoumané slovo\tnalezené slovo\n")
         f.write("*\n")
         f.write(f"*rodič není {rod} rodu\n")
         for i in spatny_rod:
-           f.write(f"{i[0]}\t{i[1]}\n")
+           f.write(f"{varianta1}\t{i[0]}\t{i[1]}\n")
         f.write("*\n")
         f.write(f"*rodič a slovo jsou vzdáleně příbuzní\n")
         for i in vzdalene:
-           f.write(f"{i[0]}\t{i[1]}\n")
+           f.write(f"{varianta2}\t{i[0]}\t{i[1]}\n")
         f.write("*\n")
         f.write(f"*možný předek {rod} rodu\n")
         for i in dobry_rod:
-            f.write(f"{i[0]}\t{i[1]}\n")
+            f.write(f"{varianta2}\t{i[0]}\t{i[1]}\n")
         f.write(f"*možný předek, který není {rod} rodu\n")
         f.write("*\n")
         for i in spatny_nepripojeno:
-            f.write(f"{i[0]}\t{i[1]}\n")
+            f.write(f"{varianta2}\t{i[0]}\t{i[1]}\n")
         f.write("*\n")
         f.write("*předek nenalezen\n")
         for i in bez:
@@ -115,7 +133,7 @@ def je_spravne_bez_rodice(bez):
 
 def muze_se_pridat(rodic, lexeme, seznam_dobry, seznam_spatny, lexicon):
     if rodic != lexeme.lemma:
-        if kontrola_rodice(rodic, lexeme):
+        if kontrola_rodice(rodic, lexeme, lexicon):
             seznam_dobry.append((lexeme.lemma, rodic))
         else: 
             seznam_spatny.append((lexeme.lemma, rodic))
@@ -160,6 +178,31 @@ def tvorba_moznych_variant(slovicko, all_lemmas, vzorkar, dite, lexicon):
     
     return seradit_moznosti(moznosti, lexicon)
 
+def tvorba_moznych_variant_tri(slovicko, all_lemmas, vzorkar, dite, pismeno, lexicon):
+    moznosti = []
+    if vzorkar == 1:
+        for i in "aeio":
+            slovo = slovicko + i + pismeno
+
+            if slovo in all_lemmas and slovo != dite:
+                moznosti.append(slovo)
+
+    elif vzorkar == 0:
+        for i in "aeio":
+            slovo = slovicko + i 
+
+            if slovo in all_lemmas and slovo != dite:
+                moznosti.append(slovo)
+    
+    else:
+        for i in "aeio":
+            slovo = slovicko + "k" + i 
+
+            if slovo in all_lemmas and slovo != dite:
+                moznosti.append(slovo)
+    
+    return seradit_moznosti(moznosti, lexicon)
+
 def nejlepsi(kandidati, lexicon):
     hodnota = 0
     nejvyssi = None 
@@ -174,6 +217,20 @@ def nejlepsi(kandidati, lexicon):
 
     return nejvyssi 
 
+def nejlepsi_noun(kandidati, lexicon):
+    hodnota = 0
+    nejvyssi = None
+
+    if len(kandidati) > 0:
+        for i in kandidati:
+            if i is not None:
+                slovo = lexicon.get_lexemes(i)[0]
+                if slovo.misc['corpus_stats']['relative_frequency'] > hodnota and slovo.pos == "NOUN":
+                    nejvyssi = slovo.lemma
+                    hodnota = slovo.misc['corpus_stats']['relative_frequency']    
+
+    return nejvyssi 
+
 
 def tvorba_novych_variant_kratke(nove, all_lemmas):
     kandidati = set()
@@ -184,6 +241,22 @@ def tvorba_novych_variant_kratke(nove, all_lemmas):
                 kandidati.add(slovicko)
     return kandidati
 
+def levenhstein(prvni, druhy):
+    n = len(prvni)
+    m = len(druhy)
+    tabulka = [[0] * (m + 2) for _ in range(n + 2)]
+    for i in range(1, n+ 2):
+        tabulka[i][m+1] = n - i + 1
+    for j in range(1, m + 2):
+        tabulka[n+ 1][j] = m - j + 1
+    for i in range(n, 0, -1):
+        for j in range(m, 0, -1):
+            delta = 1
+            if prvni[i - 1] == druhy[j-1]:
+                delta = 0
+            tabulka[i][j] = min(delta + tabulka[i + 1][j + 1], 1 + tabulka[i + 1][j], 1 + tabulka[i][j + 1])
+
+    return tabulka[1][1]
 
 def vzdalene_pribuzny(dite, rodic):
     daleko = False
